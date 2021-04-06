4142934f56aa8f0e6f251fca3e0512312e6c0870
Muujii <muujii.tsend@gmail.com>
2021-04-06 11:18:41 -0700

    shoog shiddeg bolgoh

--- app.js
+++ app.js
@@ -16,17 +16,21 @@ let roundScore = 0;
 
 // Shoonii ali talaaraa buusniig hadgalah huvisagch heregtei, 1-6 gesen utgiig ene huvisagchid sanamsargvigeer uusgej ogno.
 
-let dice = Math.floor(Math.random() * 6) + 1;
 
 // window.document.querySelector("#score-0").textContent = dice;
 // document.querySelector("#score-1").innerHTML = "<em>dice</em>";
 
-document.querySelector("#score-0").textContent = 0;
-document.querySelector("#score-1").textContent = 0;
+document.getElementById("score-0").textContent =0;
+document.getElementById("score-1").textContent = 0;
+document.getElementById("current-0").textContent = 0;
+document.getElementById("current-1").textContent = 0;
 
-document.querySelector("#current-0").textContent = 0;
-document.querySelector("#current-1").textContent = 90;
+let diceDom = document.querySelector(".dice");
 
-document.querySelector(".dice").style.display = "none";
+diceDom.style.display = "none";
 
-console.log("Shoo: " + dice);
+document.querySelector(".btn-roll").addEventListener("click", function() {
+    let diceNumber = Math.floor(Math.random() * 6) + 1;
+    diceDom.style.display = "block";
+    diceDom.src = "dice-" + diceNumber + ".png";
+});
