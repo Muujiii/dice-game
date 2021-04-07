e89c48dbcb321bd7851e7ff9105e60732977ef03
Muujii <muujii.tsend@gmail.com>
2021-04-07 11:39:29 -0700

    Toglogchiin eeljiig solidog bolno

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // Toglogchiin eeljiig hadgalah huvisagch, 1r toglogchiig 0,  2r toglogchiig 1 gej temdegley.
 
-let activePlayer = 1;
+let activePlayer = 0;
 
 
 
@@ -29,8 +29,47 @@ let diceDom = document.querySelector(".dice");
 
 diceDom.style.display = "none";
 
+// Shoog shideh event listener
 document.querySelector(".btn-roll").addEventListener("click", function() {
+    // 1-6 iin random toog gargaj avna
     let diceNumber = Math.floor(Math.random() * 6) + 1;
+
+    // Shoonii zurgiig web deer gargaj irne.
     diceDom.style.display = "block";
+
+    // Buusan random toond hargalzah shoonii zyrag web deer gargaj irne
     diceDom.src = "dice-" + diceNumber + ".png";
+
+    // Buusan too ni 1ees yalgaatai bol idevhitei Toglogchiin eeljiin onoog nemegdvvlne
+    if (diceNumber !== 1) {
+        // 1-ees yalgaatai buulaa.Buusan toog toglogchid nemj ogno
+        roundScore = roundScore + diceNumber;
+        document.getElementById("current-" + activePlayer).textContent = roundScore;
+    } else {
+        // 1 buusan tul toglogchiin eeljiig ene hesegt solij ogno.
+        // Herev idevhitei toglogch ni 0 baival idevhitei toglogchiig 1 bolgono
+        // Ugui bol idevhitei toglogchiig 0 bolgono
+
+        scores[activePlayer] += roundScore;
+
+        // Ene toglogchiin eeljindee tsugluulsan onoog 0 bolgono
+        roundScore = 0;
+        document.getElementById("current-" + activePlayer).textContent = 0;
+        document.getElementById("score-" + activePlayer).textContent =scores[activePlayer];
+
+        // Toglogchoo solij bn
+        activePlayer = (activePlayer === 0 ? 1 : 0);
+
+        // Ulaan tsegiig shiljvvleh
+        document.querySelector(".player-0-panel").classList.toggle("active");
+        document.querySelector(".player-1-panel").classList.toggle("active");
+
+        // Shoog tvr alga bolgono (1 buusan ued)
+        diceDom.style.display = "none";
+
+
+    }
+
+
+
 });
